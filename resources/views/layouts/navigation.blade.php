35b4b0f74375a731c323960454f423b4e46e3896
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2024-01-26 15:27:59 +0300

    officially working on this project

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,17 +6,17 @@
                 <!-- Logo -->
                 
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('redirect') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <!-- <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('redirect')" :active="request()->routeIs('redirect')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                </div> -->
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -68,7 +68,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link :href="route('redirect')" :active="request()->routeIs('redirect')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth','verified')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/redirect', 'App\Http\Controllers\HomeController@redirect')->name('redirect');
+Route::get('/redirect', 'App\Http\Controllers\HomeController@redirect')->middleware('auth')->name('redirect');
 
 require __DIR__.'/auth.php';
